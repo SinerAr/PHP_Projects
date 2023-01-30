67934355e7acc06a598adc989061bc248317a03b
SinerAr <sinerar@gmail.com>
2023-01-30 21:58:57 +0000

    Домашка 3 - Готово

--- homework3/index.php
+++ homework3/index.php
@@ -1,3 +1,66 @@
 <?php
-echo "111111111111";
+class Color
+{
+    private int $red;
+    private int $green;
+    private int $blue;
+
+
+    private function setRed(int $red):self
+    {
+        if (!intdiv($red, 255)) $this->red = $red;
+        else echo "Input number in range 0-255.<br>";
+        return $this;
+    }
+
+    private function setGreen(int $green):self
+    {
+        if (!intdiv($green, 255)) $this->green = $green;
+        else echo "Input number in range 0-255.<br>";
+        return $this;
+    }
+
+    private function setBlue(int $blue):self
+    {
+        if (!intdiv($blue, 255)) $this->blue = $blue;
+        else echo "Input number in range 0-255.<br>";
+        return $this;
+    }
+
+    public function __construct($red, $green, $blue)
+    {
+        $this->setRed($red)
+            ->setGreen($green)
+            ->setBlue($blue);
+    }
+
+    private function getRed():int
+    {
+        return $this->red;
+    }
+
+    private function getGreen():int
+    {
+        return $this->green;
+    }
+
+    private function getBlue():int
+    {
+        return $this->blue;
+    }
+
+    public function mixColor(Color $color):self
+    {
+        $this->setRed(($this->getRed()+$color->getRed())/2);
+        $this->setGreen(($this->getGreen()+$color->getGreen())/2);
+        $this->setBlue(($this->getBlue()+$color->getBlue())/2);
+        return $this;
+    }
+}
+
+$color1 = new Color(10, 20, 30);
+$color2 = new Color(90, 200, 80);
+
+$mixedColor = $color2->mixColor($color1);
+print_r($mixedColor);
 ?>
